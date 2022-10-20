cd4aab6e6a012f74433343282d14e27555f5357e
athowes <adamhowes@outlook.com>
2022-10-20 14:09:46 -0400

    inla-tmb-constraints to .Rmd

--- make.R
+++ make.R
@@ -1,4 +1,4 @@
 knitr::spin("arima110-inla-rgeneric.R")
 knitr::render("inla-internals.Rmd")
-knitr::spin("inla-tmb-constraints.R")
+knitr::render("inla-tmb-constraints.Rmd")
 knitr::render("tmb-bym2.Rmd")
